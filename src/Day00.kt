44beef00580d3ae87699793ae89fc9c137966c69
Roman Kalashnikov <roman@ka.pp.ru>
2023-12-11 20:02:28 +0300

    Fix template

--- src/Day00.kt
+++ src/Day00.kt
@@ -1,6 +1,6 @@
-const val DAY = "00"
-
 fun main() {
+    val day = "00"
+
     fun part1(input: List<String>): Int {
         return 0
     }
@@ -10,15 +10,15 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day${DAY}_1_test")
+    val testInput1 = readInput("Day${day}_1_test")
     val part1result = part1(testInput1)
     check(part1result == 8) { "Got $part1result" }
 
-    val testInput2 = readInput("Day${DAY}_2_test")
+    val testInput2 = readInput("Day${day}_2_test")
     val part2result = part2(testInput2)
     check(part2result == 2286) { "Got $part2result" }
 
-    val input = readInput("Day${DAY}")
+    val input = readInput("Day${day}")
     part1(input).println()
     part2(input).println()
 }
